e3eadf6fe16cc20ecffd659069f359061b38b3fa
{{Dev.Habeeb}} <olaitan6201@gmail.com>
2023-10-05 16:42:42 +0100

    updated Guest Layout component

--- src/pages/layouts/GuestLayout.jsx
+++ src/pages/layouts/GuestLayout.jsx
@@ -4,7 +4,7 @@ import { Outlet } from 'react-router-dom'
 // Pages that shouldn't be accessible after successful authentication 
 export default function GuestLayout() {
 	return (
-		<div>
+		<div className='max-w-lg mx-auto bg-light min-h-screen px-4 py-8 shadow-lg border rounded-xl flex flex-col items-center gap-8'>
 			<Outlet />
 		</div>
 	)
