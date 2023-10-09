61a2d3be417d144ee1ca59b63917bfb28eae4b58
Onakoya Kayode <macbook@Onakoya-Kayode.local>
2023-10-09 16:23:53 +0100

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Home from './pages/Home'
 import DashboardHome from './pages/DashboardHome/DashboardHome'
 import RaffleStake from './component/DashboardRoute/RaffleStake/RaffleStake'
 import StakePrice from './component/DashboardRoute/StakePrice/StakePrice'
+import Settings from './component/DashboardRoute/Settings/Settings'
 
 function App() {
     return (
@@ -25,6 +26,7 @@ function App() {
                     element={<RaffleStake />}
                 />
                 <Route path="/dashboard/stakePrice" element={<StakePrice />} />
+                <Route path="/dashboard/settings" element={<Settings />} />
             </Routes>
         </Router>
     )
