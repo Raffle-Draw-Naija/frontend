733d9206c0c8ef0a1803162d8f3197c2b2d12eb1
Onakoya Kayode <macbook@Onakoya-Kayode.local>
2023-10-18 14:47:20 +0100

    add navbar

--- src/component/DashboardRoute/RaffleStake/RaffleStake.jsx
+++ src/component/DashboardRoute/RaffleStake/RaffleStake.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Dashboard from '../../Dashboard/Dashboard'
+import Navbar from '../../../pages/layouts/Navbar/Navbar'
 
 const RaffleStake = () => {
     return (
         <Dashboard>
             <div className="raffle-stake">
+                <Navbar />
                 <h1>Raffle Stake</h1>
             </div>
         </Dashboard>
