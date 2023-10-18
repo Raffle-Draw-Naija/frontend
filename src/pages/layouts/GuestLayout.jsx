04adb40664ef1d2da485c8bedd127348440e7096
{{Dev.Habeeb}} <olaitan6201@gmail.com>
2023-10-18 09:47:39 +0100

    re-designed login page

--- src/pages/layouts/GuestLayout.jsx
+++ src/pages/layouts/GuestLayout.jsx
@@ -4,8 +4,11 @@ import { Outlet } from 'react-router-dom'
 // Pages that shouldn't be accessible after successful authentication 
 export default function GuestLayout() {
 	return (
-		<div className='max-w-lg mx-auto bg-light min-h-screen px-4 py-8 shadow-lg border rounded-xl flex flex-col items-center gap-8'>
-			<Outlet />
+		<div className="flex flex-col items-center justify-center w-full min-h-screen">
+			<div className='w-11/12 sm:w-7/12 md:w-6/12 lg:w-4/12 max-w-lg mx-auto px-4 py-8 border-none flex flex-col items-center justify-center gap-8'>
+				<img src="/logo.png" alt="Logo" className='w-[143px] h-[117px]' />
+				<Outlet />
+			</div>
 		</div>
 	)
 }
