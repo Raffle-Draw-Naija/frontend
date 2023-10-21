4f838df14899bd7b5b30432ce87cabd1b539760d
Samson <chuksdsilent@gmail.com>
2023-10-21 11:52:07 +0100

    completed conflict

---
+++ src/router.jsx
@@ -0,0 +1,78 @@
+import { createBrowserRouter } from "react-router-dom";
+import NotFound from "./pages/errors/NotFound";
+import SignIn from "./pages/auth/SignIn";
+import GuestLayout from "./pages/layouts/GuestLayout";
+import Index from "./pages/Index";
+import Layout from "./component/Layouts/Layout";
+import Dashboard from "./pages/layouts/MainContent/Dashboard/Dashboard";
+import RaffleStake from "./pages/layouts/MainContent/RaffleStake/RaffleStake";
+import WinningTags from "./pages/layouts/MainContent/WinningTags/WinningTags";
+import EditWinningTags from "./pages/layouts/MainContent/WinningTags/EditWinningTags";
+import StartADraw from "./pages/layouts/MainContent/StartADraw/startADraw";
+import { ToastContainer, toast } from 'react-toastify';
+import ActiveRaffleDraws from "./pages/layouts/MainContent/StartADraw/ActiveRaffleDraws";
+import BankAccounts from "./pages/layouts/BankAccounts/BankAccounts";
+
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <Index />,
+	},
+	{
+		path: "/",
+		element: <GuestLayout />,
+		children: [
+			{
+				path: "/login",
+				element: <SignIn />,
+			},
+		],
+	},
+	{
+		path: "/",
+		element: <Layout />,
+		children: [
+			{
+				path: "/dashboard",
+				element: <Dashboard />
+
+			},
+			{
+				path: "/raffle-stakes",
+				element: <RaffleStake />
+
+			},
+			{
+				path: "/raffle-draws",
+				element: <ActiveRaffleDraws />
+
+			},
+			{
+				path: "/winning-tags",
+				element: <WinningTags />
+			},
+			{
+				path: "/winning-tag/edit/:id",
+				element: <EditWinningTags />
+
+			},
+			{
+				path: "/start-a-draw",
+				element: <StartADraw />
+
+			},
+			{
+				path: "/bank-accounts",
+				element: <BankAccounts />
+
+			}
+		],
+	},
+	{
+		path: "*",
+		element: <NotFound />,
+	},
+])
+
+export default router
