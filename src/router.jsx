99cc0d796f0fdea0ce5433443529fd70714ce08a
Samson <chuksdsilent@gmail.com>
2023-10-25 09:32:58 +0100

    new changes

--- src/router.jsx
+++ src/router.jsx
@@ -9,85 +9,84 @@ import RaffleStake from "./pages/layouts/MainContent/RaffleStake/RaffleStake";
 import WinningTags from "./pages/layouts/MainContent/WinningTags/WinningTags";
 import EditWinningTags from "./pages/layouts/MainContent/WinningTags/EditWinningTags";
 import StartADraw from "./pages/layouts/MainContent/StartADraw/startADraw";
-import { ToastContainer, toast } from 'react-toastify';
 import ActiveRaffleDraws from "./pages/layouts/MainContent/StartADraw/ActiveRaffleDraws";
 import BankAccounts from "./pages/layouts/BankAccounts/BankAccounts";
 import ProtectedRoute from "./components/ProtectedRoute";
 
 
 const router = createBrowserRouter([
-	{
-		path: "/",
-		element: <Index />,
-	},
-	{
-		path: "/",
-		element: <GuestLayout />,
-		children: [
-			{
-				path: "/login",
-				element: <SignIn />,
-			},
-		],
-	},
-	{
-		path: "/",
-		element: (
-			<ProtectedRoute>
-				<Layout />
-			</ProtectedRoute>
-		),
-		children: [
-			{
-				path: "/dashboard",
-				element: <Dashboard />
+    {
+        path: "/",
+        element: <Index />,
+    },
+    {
+        path: "/",
+        element: <GuestLayout />,
+        children: [
+            {
+                path: "/login",
+                element: <SignIn />,
+            },
+        ],
+    },
+    {
+        path: "/",
+        element: (
+            <ProtectedRoute>
+                <Layout />
+            </ProtectedRoute>
+        ),
+        children: [
+            {
+                path: "/dashboard",
+                element: <Dashboard />
 
-			},
-			{
-				path: "/raffle-stakes",
-				element: <RaffleStake />
+            },
+            {
+                path: "/raffle-stakes",
+                element: <RaffleStake />
 
-			},
-			{
-				path: "/raffle-draws",
-				element: <ActiveRaffleDraws />
+            },
+            {
+                path: "/raffle-draws",
+                element: <ActiveRaffleDraws />
 
-			},
-			{
-				path: "/winning-tags",
-				element: <WinningTags />
-			},
-			{
-				path: "/winning-tag/edit/:id",
-				element: <EditWinningTags />
+            },
+            {
+                path: "/winning-tags",
+                element: <WinningTags />
+            },
+            {
+                path: "/winning-tag/edit/:id",
+                element: <EditWinningTags />
 
-			},
-			{
-				path: "/start-a-draw",
-				element: <StartADraw />
+            },
+            {
+                path: "/start-a-draw",
+                element: <StartADraw />
 
-			},
-			{
-				path: "/payment-by-card",
-				element: <BankAccounts />
+            },
+            {
+                path: "/payment-by-card",
+                element: <BankAccounts />
 
-			},
-			{
-				path: "/payment-by-credit",
-				element: <BankAccounts />
+            },
+            {
+                path: "/payment-by-credit",
+                element: <BankAccounts />
 
-			},
-			{
-				path: "/wallet",
-				element: <BankAccounts />
+            },
+            {
+                path: "/wallet",
+                element: <BankAccounts />
 
-			}
-		],
-	},
-	{
-		path: "*",
-		element: <NotFound />,
-	},
+            }
+        ],
+    },
+    {
+        path: "*",
+        element: <NotFound />,
+    },
 ])
 
 export default router
